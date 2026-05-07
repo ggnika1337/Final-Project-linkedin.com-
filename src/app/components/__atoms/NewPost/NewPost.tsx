82171153f20d0e2df8501d9aed9e3f3fd5397b67
ggnika1337 <nikushanika568@gmail.com>
2026-05-07 16:10:02 +0400

    Feat: Visually updated

--- src/app/components/__atoms/NewPost/NewPost.tsx
+++ src/app/components/__atoms/NewPost/NewPost.tsx
@@ -12,14 +12,23 @@ function NewPost() {
             Start a post
           </button>
         </div>
-        <div className="w-full h-full items-center justify-between flex font-[600] whitespace-nowrap px-15 max-[830px]:px-0 pt-2">
-          <div className="hover:bg-[#6e732111] items-center justify-center flex p-3 rounded-[10px]">
+        <div className="w-full h-full cursor-pointer items-center justify-between flex font-[600] whitespace-nowrap px-15 max-[830px]:px-0 pt-2">
+          <div className="hover:bg-[#6e73211f] items-center justify-center flex p-3 rounded-[10px]">
+            <svg fill="green" className="w-[25px] h-[20px] mr-1">
+              <path d="M19 4H5a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3m-9 12V8l6 4z"></path>
+            </svg>
             <h1>Video</h1>
           </div>
-          <div className="hover:bg-[#6e732111] items-center justify-center flex p-3 rounded-[10px]">
+          <div className="hover:bg-[#6e73211f] cursor-pointer items-center justify-center flex p-3 rounded-[10px]">
+            <svg fill="#0b67c2" className="w-[25px] h-[20px] mr-1">
+              <path d="M19 4H5a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3m1 13a1 1 0 0 1-.29.71L16 14l-2 2-6-6-4 4V7a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1zm-2-7a2 2 0 1 1-2-2 2 2 0 0 1 2 2s"></path>
+            </svg>
             <h1>Photo</h1>
           </div>
-          <div className="hover:bg-[#6e732111] items-center justify-center flex p-3 rounded-[10px]">
+          <div className="hover:bg-[#6e73211f] cursor-pointer items-center justify-center flex p-3 rounded-[10px]">
+            <svg fill="#b34120" className="w-[25px] h-[20px] mr-1">
+              <path d="M21 3v2H3V3zm-6 6h6V7h-6zm0 4h6v-2h-6zm0 4h6v-2h-6zM3 21h18v-2H3zM13 7H3v10h10z"></path>
+            </svg>
             <h1>Write article</h1>
           </div>
         </div>
